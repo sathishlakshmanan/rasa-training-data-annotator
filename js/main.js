48a8da419917528aa029573909967277f3dfa974
sathishlakshmanan <sathishklakshmanan@gmail.com>
2023-06-04 22:07:28 +0200

    js to get the selected text from textarea

---
+++ js/main.js
@@ -0,0 +1,85 @@
+// TODO: show warning on page refresh
+// window.onbeforeunload = () => {
+//   return "Your date will be lost, are you sure?";
+// };
+
+const btn = document.getElementById('submit');
+const form = document.getElementById('entity-form');
+console.log(btn);
+
+
+let createButton = (parsedJsonData) => {
+    const someDiv = document.getElementById("entity-buttons");
+    const btn = document.createElement("BUTTON");
+    btn.innerHTML = parsedJsonData;
+    someDiv.appendChild(btn);
+    console.log("---called successfully---");
+};
+
+const newBtn = document.getElementById('entity-buttons');
+newBtn.onclick = function(){
+    var txtarea = document.getElementById("textarea-left");
+
+    var len = txtarea.value.length;
+
+    // Obtain the index of the first selected character
+    var start = txtarea.selectionStart;
+
+    // Obtain the index of the last selected character
+    var finish = txtarea.selectionEnd;
+
+    // Obtain the selected text
+    var sel = txtarea.value.substring(start, finish);
+    var replace = "---" + sel + "---";
+    // var replace = newBtn.textContent;
+    txtarea.value =  txtarea.value.substring(0,start) + replace  + txtarea.value.substring(finish,len);
+
+    console.log(sel);
+    console.log(newBtn.innerText);
+    console.log(newBtn.textContent);
+}
+
+function getSelection(){
+    var txtarea = document.getElementById("textarea-left");
+
+    var len = txtarea.value.length;
+
+    // Obtain the index of the first selected character
+    var start = txtarea.selectionStart;
+
+    // Obtain the index of the last selected character
+    var finish = txtarea.selectionEnd;
+
+    // Obtain the selected text
+    var sel = txtarea.value.substring(start, finish);
+    var replace = "---" + sel + "---";
+    txtarea.value =  txtarea.value.substring(0,start) + replace + txtarea.value.substring(finish,len);
+    return sel
+}
+
+form.addEventListener('submit', (e) => {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // show the form values
+    const formData = new FormData(form);
+    const values = [...formData.entries()];
+    console.log(values);
+
+    const data = Object.fromEntries(formData);
+    const jsonData = JSON.stringify(data);
+    const parsedJsonData = JSON.parse(jsonData);
+    console.log(parsedJsonData);
+    console.log(parsedJsonData["entity"]);
+    // if(!parsedJsonData["entity"] == "" && !parsedJsonData["value"] == "" && !parsedJsonData["group"] == "" && !parsedJsonData["role"] == ""){
+    //     document.getElementById('textarea-up').value = jsonData;
+    //     createButton(jsonData);
+    // }
+    if(parsedJsonData["entity"] == "" && parsedJsonData["value"] == "" && parsedJsonData["group"] == "" && parsedJsonData["role"] == ""){
+        console.log("Everything is empty");
+        return 0;
+    }
+    document.getElementById('textarea-up').value = jsonData;
+    createButton(jsonData);
+});
+
